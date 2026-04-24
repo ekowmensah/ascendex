5a5b7341c093ca2b080f746be7a37180894b8542
ekowmensah <ekowme@gmail.com>
2026-04-24 07:20:29 +0000

    git push

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ $title ?? 'AscendEX Binary' }}</title>
+    <title>{{ $title ?? 'Trans Market Group' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="min-h-screen text-slate-100">

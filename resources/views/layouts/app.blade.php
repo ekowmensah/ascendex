cae59333b115167099681bda3da4dbe6df5c8698
ekowmensah <ekowme@gmail.com>
2026-04-24 07:38:53 +0000

    git push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
     <title>{{ $title ?? 'TransMarket' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
